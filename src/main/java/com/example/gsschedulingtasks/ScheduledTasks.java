b8db8ec332ac1b27c54722a9f2ae217bd1bb4888
Stanisław Serwin <stanserw@wp.pl>
2022-03-16 23:04:35 +0100

    scheduled in cron

--- src/main/java/com/example/gsschedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/gsschedulingtasks/ScheduledTasks.java
@@ -14,7 +14,10 @@ public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRate = 5000)
+//    @Scheduled(cron = "0/45 * * * * *") // w 0 i 45 sekundzie
+//    @Scheduled(cron = "0 0 8 * * *") // o 8:00 kazdego dnia
+    @Scheduled(cron = "*/15 * * * * *") // co 15 sekund
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
