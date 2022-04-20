66975c6d216fe813579bcdebeafd1e7a94e44b09
Stanisław Serwin <stanislaw.serwin@polskapress.pl>
2022-04-20 08:58:11 +0200

    modify in ScheduledTasks

--- src/main/java/com/example/gsschedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/gsschedulingtasks/ScheduledTasks.java
@@ -15,9 +15,9 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 //    @Scheduled(fixedRate = 5000)
-//    @Scheduled(cron = "0/45 * * * * *") // w 0 i 45 sekundzie
-//    @Scheduled(cron = "0 0 8 * * *") // o 8:00 kazdego dnia
-    @Scheduled(cron = "*/15 * * * * *") // w kazdej sekundzie podzielnej przez 15
+//    @Scheduled(cron = "0/45 * * * * *") // 0 i 45 a second
+//    @Scheduled(cron = "0 0 8 * * *") // o 8:00 every day
+    @Scheduled(cron = "*/15 * * * * *") // every second divisible by 15
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
